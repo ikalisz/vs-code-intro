ff757afa1f53be587b7d1ef3fe98ed24858b83f0
Ian Kaliszewski <ikalisz@yahoo.com>
2019-04-18 09:48:36 -0600

    finished most problems

--- Key Board Cardio/01-Basic-Editing/basic.js
+++ Key Board Cardio/01-Basic-Editing/basic.js
@@ -1,35 +1,35 @@
 // Pressing one key combination delete the word 'Ross'
-var name = "Bob Ross";
+var name = "Bob ";
 
 // Pressing two key combinations delete the words 'best man'
-var description = "Is the best man";
+var description = "Is the ";
 
 // Pressing one key combination select the whole quote 
 // then delete it
-var lifeStory = "A man born of humble means ventures into the forrest and comes out a legend";
+
 
 // Pressing three key combinations highlight the words "never ever"
 // Cut the selection
-var quote = "This ship will never ever sink";
+var quote = "This ship will  sink";
 
 // Now paste your selection inside the quote before 'land'
-var location = ' land'
+var location = 'never ever land'
 
 // select the words coming the equal sign using one key combination
 // Wrap it in quotes using one key
-var poem = roses are red, this poem is great, how about you
+var poem = 'roses are red, this poem is great, how about you'
 
 // starting at the opening quote and pressing two key combinations delete the words 'can haz'
-var cat = 'can haz cheeseburger'
+var cat = ' cheeseburger'
 
 // Delete all repeat words, using one key combination per word
-var lyrics = 'sweet dreams dreams are made made of these these'
+var lyrics = 'sweet  dreams are  made of  these'
 
 // Using two key combinations select everything inside the for loop's () curly braces
 // Then delete it all
 for (let i = 0; i < 50; i++) {
     if (i % 2 === 0) {
-        console.log('its even!')
+        console.log() 
     }
 }
 
@@ -37,13 +37,13 @@ for (let i = 0; i < 50; i++) {
 // then delete it
 for (let i = 0; i < 50; i++) {
     if (i % 2 === 0) {
-        console.log('its even!')
+        console.log() 
     }
 }
 
 // using two key combinations select everything inside of the personMaker Parameters (name, age, info)
 // then delete it
-function personMaker(name, age, info) {
+function personMaker() {
     var obj = {
         name: name,
         age: age,
@@ -58,36 +58,36 @@ function personMaker(name, age, info) {
 // delete one of the duplicate lines using only one key combination
 function incrementor(num) {
     num += 1;
-    num += 1;
     return num;
 }
 
 // pressing only one key combinations select all the words to the left of log
 // add () parenthesis the selected words using only one key
 // also add '' quotes to the selected text using only one key
-console.log richard simmons is the man
+console.log(' richard simmons is the man')
 
 
 // using only three key combinations select 'x > y'
 // Then wrap it in () parenthesis using one key
-if x > y {
+if (x > y) {
     console.log('x is bigger')
 }
 
 // pressing two key combinations select all the text under the function delectation
 // wrap it all in {} curly braces
-function superFun() 
+function superFun(){ 
     var randomNumber = Math.random();
+}
     return randomNumber
 
 // add commas after each quote except the last, using only 7 key combinations
 // pressing one key combination select all the text to the left of the =
 // then pressing one key wrap it all in [] square brackets.
-var people = 'Bob Ross' 'Richard Simmons' 'Bob Marley' 'Tim the wizard'
+var people =[ 'Bob Ross', 'Richard Simmons', 'Bob Marley', 'Tim the wizard',]
 
 
 // delete the console.log using only one key stroke
-console.log('I will never be defeated');
+('I will never be defeated');
 
 
 // select the line that returns 'Red' using only one key combination
@@ -98,9 +98,9 @@ function alwaysRed(number) {
         return 'Red'
     }
     if(number === 0) {
-
+        return 'Red'
     }
     if (number < 0) {
-
+        return 'Red'
     }
 }
